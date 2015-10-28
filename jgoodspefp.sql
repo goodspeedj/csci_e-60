9c3041eef0c4713183c70f16beb03e20198435ea
Jim Goodspeed <james.goodspeed@libertymutual.com>
2015-10-28 16:22:27 -0400

    Create table statememts working

---
+++ jgoodspefp.sql
@@ -0,0 +1,134 @@
+-- ******************************************************
+-- 2015fp.sql
+--
+-- Loader for Final Project Database
+--
+-- Description: This script contains the DDL to load
+--              the tables of the
+--              PEASEWATER database
+--
+-- Author:  James Goodspeed
+--
+-- Student:  James Goodspeed
+--
+-- Date:   October, 2015
+--
+-- ******************************************************
+
+-- ******************************************************
+--    SPOOL SESSION
+-- ******************************************************
+
+spool 2015fp.lst
+
+-- ******************************************************
+--    DROP TABLES
+-- ******************************************************
+DROP table tbAddress purge;
+DROP table tbWellSample purge;
+DROP table tbPersonPFCLevel purge;
+DROP table tbStudyPFCLevel purge;
+DROP table tbPerson purge;
+DROP table tbWell purge;
+DROP table tbStudy purge;
+DROP table tbChemical purge;
+
+-- ******************************************************
+--    DROP SEQUENCES
+-- ******************************************************
+DROP sequence seq_address;
+DROP sequence seq_person;
+DROP sequence seq_chemical;
+DROP sequence seq_well;
+DROP sequence seq_sample;
+DROP sequence seq_study;
+
+-- ******************************************************
+--    CREATE TABLES
+-- ******************************************************
+
+
+CREATE table tbChemical (
+        chemID          number(11,0)            not null
+            constraint pk_chemical primary key,
+        shortName       varchar2(15)            not null,
+        longName        varchar2(40)            not null,
+        epaPHALevel     number(3,2)             not null
+);
+
+
+CREATE table tbStudy (
+        studyID         number(11,0)            not null
+            constraint pk_study primary key,
+        name            varchar2(25)            not null,
+        startDate       date                    null,
+        endDate         date                    null,
+        participants    number(11,0)            not null
+);
+
+
+CREATE table tbWell (
+        wellID          number(11,0)            not null
+            constraint pk_well primary key,
+        name            varchar2(20)            not null,
+        location        varchar2(30)            not null,
+        active          char(1)                 not null,
+            constraint  rg_active check (REGEXP_LIKE(active, '^Y|N$'))
+);
+
+
+CREATE table tbPerson (
+        personID        number(11,0)            not null
+            constraint pk_person primary key,
+        nhHHSID         varchar2(10)            not null,
+        age             number(3,0)             not null,
+        yearsExposed    number(2,0)             not null,
+        sex             char(1)                 not null,
+            constraint  rg_sex check (REGEXP_LIKE(sex, '^M|F$'))
+);
+
+
+CREATE table tbStudyPFCLevel (
+        studyID         number(11,0)            not null
+            constraint  fk_studyID_tbStudyPFCLevel references tbStudy (studyID),
+        chemID          number(11,0)            not null
+            constraint  fk_chemID_tbStudyPFCLevel references tbChemical (chemID),
+        pfcLevel           number(3,3)             not null,
+            constraint  pk_studypfclevel primary key (studyID, chemID)
+
+);
+
+
+CREATE table tbPersonPFCLevel (
+        personID        number(11,0)            not null
+            constraint  fk_personID_tbPersonPFCLevel references tbPerson (personID),
+        chemID          number(11,0)            not null
+            constraint  fk_chemID_tbPersonPFCLevel references tbChemical (chemID),
+        pfcLevel           number(3,3)             not null,
+            constraint  pk_personpfclevel primary key (personID, chemID)
+
+);
+
+
+CREATE table tbWellSample (
+        sampleID        number(11,0)            not null
+            constraint pk_wellsample primary key,
+        wellID          number(11,0)            not null
+            constraint  fk_wellID_tbWellSample references tbWell (wellID),
+        wellID          number(11,0)            not null
+            constraint  fk_wellID_tbWellSample references tbChemical (chemID),
+        sampleDate      date                    not null,
+        pfcLevel           number(3,3)             not null,
+            constraint  pk_personpfclevel primary key (personID, chemID)
+
+);
+
+
+CREATE table tbAddress (
+        addressID        number(11,0)            not null
+            constraint pk_address primary key,
+        personID         number(11,0)            not null
+            constraint  fk_personID_tbAddress references tbPerson (personID),
+        address          varchar(45)             not null
+
+);
